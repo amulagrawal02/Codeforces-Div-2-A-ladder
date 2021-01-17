eb5d29fac75bfdf7c40476ad758f835f1d18b0f7
Amul <amulagrawal32@gmail.com>
2021-01-17 20:54:52 +0530

    Helpful Maths
    Signed-off-by: Amul <amulagrawal32@gmail.com>

--- Helpful_math.cpp
+++ Helpful_math.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace  std;
 int main()
 {
-    string s = "l+m+u+e";
-    sort(s.begin(),s.end(),2);
-    cout<<s; 
+    vector<char> v;
+    string s;
+    cin>>s;
+    for(int i = 0 ; i<s.size();i = i + 2)
+    {
+        v.push_back(s[i]);
+    }
+    sort(v.begin(),v.end());
+    int m = 0;
+    for(auto v1 : v)
+    {
+        s[m] = v1;
+        m = m + 2;
+    }
+    cout<<s;
     
-}   
+}  
